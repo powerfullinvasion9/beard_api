ae0085d546a13f9f0021406c313f018c50a8aec3
Isa Suleymanov <issashkalikimike@gmail.com>
2022-03-29 22:02:55 +0300

    initial commit

---
+++ beard/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn beard.wsgi --log-file -
--- beard/beard/settings.py
+++ beard/beard/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%0p^!g@r%7y#(!tyb9!x!b^+6fb)zi*1pyuzmdj60g!n8(j)k&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['beardapiapplication.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
